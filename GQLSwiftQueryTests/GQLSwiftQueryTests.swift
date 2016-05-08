9818b751024ee500a9bbcfeeaf2ba17f44140806
Worth Baker <wbaker@greenmtd.com>
2016-05-08 17:22:28 -0600

    added basic unit test

--- GQLSwiftQueryTests/GQLSwiftQueryTests.swift
+++ GQLSwiftQueryTests/GQLSwiftQueryTests.swift
@@ -10,27 +10,50 @@ import XCTest
 @testable import GQLSwiftQuery
 
 class GQLSwiftQueryTests: XCTestCase {
+  
+  
+  func testQueryGeneration() {
+    let pointsSubquery = GQLQueryItem(withKeyDesignation: "points", andSubqueryFields: ["id", "value"])
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    let userQueryItem = GQLQueryItem(
+      withKeyDesignation: "user",
+      andSubqueryFields: [
+        "username",
+        "email",
+        pointsSubquery
+      ]
+    )
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
+    let args = [
+      "userId": 1,
+      "token": "123456iadd"
+    ]
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+    let userQuery = GQLQuery(withSchemaType: .Query, withQueryTitle: "users", withQueryArguments: args, withQuery: userQueryItem)
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    XCTAssertEqual(userQuery.queryString, "{users(userId:1,token:123456iadd){user{username,email,points{id,value}}}}")
+  }
+  
+  func testMutationGeneration() {
+    let pointsSubquery = GQLQueryItem(withKeyDesignation: "points", andSubqueryFields: ["id", "value"])
     
+    let userQueryItem = GQLQueryItem(
+      withKeyDesignation: "user",
+      andSubqueryFields: [
+        "username",
+        "email",
+        pointsSubquery
+      ]
+    )
+    
+    let args = [
+      "userId": 1,
+      "token": "123456iadd"
+    ]
+    
+    let userQuery = GQLQuery(withSchemaType: .Mutation, withQueryTitle: "users", withQueryArguments: args, withQuery: userQueryItem)
+    
+    XCTAssertEqual(userQuery.queryString, "mutation{users(userId:1,token:123456iadd){user{username,email,points{id,value}}}}")
+  }
+  
 }
